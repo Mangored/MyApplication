db992298409aca8ae7d558a48b59fb7dbb2b846e
6272user <chainarhong2@gmail.com>
2016-09-01 15:49:49 +0700

    Project owner : add some text

--- app/src/main/java/test/myapplication/MainActivity.java
+++ app/src/main/java/test/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    //project owner: Add some text
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
